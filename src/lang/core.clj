4139fb3266f0d112faec98b051e0905137e3cacc
Ross Esmond <r.esmond@protonmail.com>
2022-06-11 13:57:52 -0500

    feat: implemented html generation for feature set
    I need to improve the dilineation of different tokens, like the
    different types of keywords. At the moment, all keywords look the same,
    which I don't think is great. I also would like for identifiers to
    change appearance based on whether or not they're being declared or
    referenced. And I would like Component and tag references to be
    distinct, which would need to be based on capitalization. I also see
    that a lot of highlighting colors the brackets and text with slightly
    different off-white colors, so I could look into that, but it would be a
    lot of work.

--- src/lang/core.clj
+++ src/lang/core.clj
@@ -2,30 +2,16 @@
   (:require [instaparse.core :as insta])
   (:require [clojure.pprint :as pp])
   (:require [clojure.core.match :refer [match]])
-  (:require [clojure.string :refer [join replace]]))
+  (:require [clojure.string :as st :refer [join lower-case]]))
 
 (defn log [what] (do (pp/pprint what) what))
 
-(defn convert [node]
-        (match node
-          [:WS & ws] (reduce
-                        #(if
-                          (and (string? (last %1)) (string? %2))
-                          (conj (vec (butlast %1)) (str (last %1) %2))
-                          (conj %1 %2))  
-                        [:WS]
-                        ws)
-          [:PlainText & pt] [:PlainText (join pt)]
-          [& items] (vec (map convert items))
-          :else node))
-
 (def insta-parser (insta/parser (slurp "./src/lang/js.bnf")))
 
-(defn parse [in] (convert (insta-parser in)))
+(defn parse [in] (insta-parser in))
 
 (defn getCode [node]
         (match node
-               [:Number n] (str n)
                [& items] (join (map getCode items))
                (s :guard string?) node
                (kw :guard keyword?) ""))
@@ -35,18 +21,25 @@
   (match in
          [(className :guard string?) & children] (span className (join (map spans children)))
          [:frag & items] (join (map spans items))
-         (s :guard string?) s))
+         (s :guard string?) (st/replace s "<" "&lt;")))
+
+(def keywords #{"function" "var" "let" "const" "return"})
 
 (defmulti htmlVisit #(cond (vector? %1) (first %1) (string? %1) :str :else %1))
 (defmethod htmlVisit ::Number [[_ n]] ["number" n])
-(defmethod htmlVisit ::no-print [[_ & items]] (vec (concat [:frag] (map htmlVisit items))))
-(defmethod htmlVisit ::VarDec [[_ s & items]] (vec (concat [:frag ["keyword" s]] (map htmlVisit items))))
-(defmethod htmlVisit ::Identifier [[_ id]] ["identifier" id])
-(defmethod htmlVisit :str [s] s)
-(defmethod htmlVisit :default [s] (log s))
+(defmethod htmlVisit ::Comment [[_ & items]] ["comment" (join items)])
+(defmethod htmlVisit ::named [[kw value]] [(lower-case (name kw)) value])
+(defmethod htmlVisit ::Prop [node]
+        (match node
+               [::Prop title "=" "\"" value "\""] [:frag ["property" title] (str "=\"" value "\"")]
+               [::Prop title "=" js] [:frag ["property" title] "=" (htmlVisit js)]))
+(defmethod htmlVisit :str [s] (if (contains? keywords s) ["keyword" s] s))
+(defmethod htmlVisit :default [s]
+        (match s
+               [_ & items] (vec (concat [:frag] (map htmlVisit items)))
+               :else (log s)))
 
-(derive ::CodeBlock ::no-print)
-(derive ::WS ::no-print)
+(derive ::Identifier ::named)
 
 (defn localize [node]
         (cond
@@ -55,10 +48,13 @@
           :else node))
 
 (defn lineify [html]
-        (str "<span class=\"line\">" (replace html "\n" "</span><br />\n<span class=\"line\">") "</span>"))
+        (str "<span class=\"line\">" (st/replace html "\n" "</span>\n<span class=\"line\">") "</span>"))
 
 (defn getHtml [node]
         (lineify (spans (htmlVisit (localize node)))))
 
-(defn -main [& args] (pp/pprint (getHtml (parse (slurp "./src/lang/target.js")))))
+(defn parser [structure]
+        (fn [code]))
+
+(defn -main [& args] (println (getHtml (parse (slurp "./src/lang/target.js")))))
 
--- test/lang/core_test.clj
+++ test/lang/core_test.clj
@@ -5,64 +5,102 @@
 (use 'clojure.test)
 
 (def cases
-  {
-   "function(){}" [:CodeBlock [:FunctionDec "function" "(" ")" "{" "}"]]
-   "function () { }" [:CodeBlock [:FunctionDec "function" [:WS " "] "(" ")" [:WS " "] "{"
-                                  [:CodeBlock [:WS " "]] "}"]]
-   "function () {\n}" [:CodeBlock [:FunctionDec "function" [:WS " "] "(" ")" [:WS " "] "{"
-                                   [:CodeBlock [:WS "\n"]] "}"]]
-   "function () {  }" [:CodeBlock [:FunctionDec "function" [:WS " "] "(" ")" [:WS " "] "{"
-                                   [:CodeBlock [:WS "  "]] "}"]]
-   "function what() { }" [:CodeBlock
-                          [:FunctionDec "function" [:WS " "] [:Identifier "what"] "(" ")"
-                           [:WS " "] "{" [:CodeBlock [:WS " "]] "}"]]
-   "var what" [:CodeBlock [:VarDec "var" [:WS " "] [:Identifier "what"]]]
-   "const what" [:CodeBlock [:VarDec "const" [:WS " "] [:Identifier "what"]]]
-   "let what" [:CodeBlock [:VarDec "let" [:WS " "] [:Identifier "what"]]]
-   "var what = 5" [:CodeBlock
-                   [:VarDec "var" [:WS " "] [:Identifier "what"] [:WS " "] "=" [:WS " "] [:Number "5"]]]
-   "const [a, b]=what" [:CodeBlock [:VarDec "const" [:WS " "]
-                                    [:Array "[" [:Identifier "a"] "," [:WS " "] [:Identifier "b"] "]"]
-                                    "=" [:Identifier "what"]]]
-   "const [a, b] = do(5)" [:CodeBlock [:VarDec "const" [:WS " "]
-                                       [:Array "[" [:Identifier "a"] "," [:WS " "] [:Identifier "b"] "]"]
-                                       [:WS " "] "=" [:WS " "]
-                                       [:Invoke [:Identifier "do"] "(" [:Number "5"] ")"]]]
-   "function () { var what }" [:CodeBlock [:FunctionDec "function" [:WS " "] "(" ")" [:WS " "] "{"
-                                           [:CodeBlock [:WS " "]
-                                            [:VarDec "var" [:WS " "] [:Identifier "what"] [:WS " "]]]
-                                           "}"]]
-   "var one\nvar two" [:CodeBlock
-                       [:VarDec "var" [:WS " "] [:Identifier "one"] [:WS "\n"]]
-                       [:VarDec "var" [:WS " "] [:Identifier "two"]]]
-   "return 5" [:CodeBlock [:Return "return" [:WS " "] [:Number "5"]]]
-   "// what\n" [:CodeBlock [:WS [:Comment "//" " what" "\n"]]]
-   "5" [:CodeBlock [:Number "5"]]
-   "what()" [:CodeBlock [:Invoke [:Identifier "what"] "(" ")"]]
-   "what(5)" [:CodeBlock [:Invoke [:Identifier "what"] "(" [:Number "5"] ")"]]
-   "foo.bar(5)" [:CodeBlock [:Invoke [:PropAccess [:Identifier "foo"] "." [:Identifier "bar"]] "(" [:Number "5"] ")"]]
-   "<div />" [:CodeBlock [:JSX [:TagClosed "<" [:Identifier "div"] [:WS " "] "/>"]]]
-   "<div>text</div>" [:CodeBlock [:JSX [:Tag "<" [:Identifier "div"] ">" [:PlainText "text"] "</" [:Identifier "div"] ">"]]]
-   "<div>{5}</div>" [:CodeBlock
-                     [:JSX [:Tag "<" [:Identifier "div"] ">" [:JS "{" [:Number "5"] "}"] "</" [:Identifier "div"] ">"]]]
-   "<div id=\"a\" />" [:CodeBlock
-                       [:JSX [:TagClosed "<" [:Identifier "div"] [:WS " "] [:Prop "id" "=" "\"" "a" "\""] [:WS " "] "/>"]]]
-   "<div id={5} />" [:CodeBlock [:JSX [:TagClosed "<" [:Identifier "div"] [:WS " "]
-                                       [:Prop "id" "=" [:JS "{" [:Number "5"] "}"]] [:WS " "] "/>"]]]
-   "<div id={5}>foobar</div>"
-   [:CodeBlock [:JSX [:Tag "<" [:Identifier "div"] [:WS " "] [:Prop "id" "=" [:JS "{" [:Number "5"] "}"]] ">"
-                      [:PlainText "foobar"] "</" [:Identifier "div"] ">"]]]
-   "<div><div>foo</div></div>"
-   [:CodeBlock [:JSX [:Tag "<" [:Identifier "div"] ">"
-                      [:Tag "<" [:Identifier "div"] ">" [:PlainText "foo"] "</" [:Identifier "div"] ">"]
-                      "</" [:Identifier "div"] ">"]]]
-   "foo.bar.baz" [:CodeBlock [:PropAccess [:Identifier "foo"] "." [:Identifier "bar"] "." [:Identifier "baz"]]]
-   "5 <= 2" [:CodeBlock [:Number "5"] [:WS " "] "<=" [:WS " "] [:Number "2"]]})
+  [
+   ["function(){}" [:CodeBlock [:FunctionDec "function" "(" ")" "{" "}"]]
+    ["line" ["keyword" "function"] "(){}"]]
+   ["function () { }" [:CodeBlock [:FunctionDec "function" [:WS " "] "(" ")" [:WS " "] "{"
+                                   [:CodeBlock [:WS " "]] "}"]]
+    ["line" ["keyword" "function"] " () { }"]]
+   ["function () {\n}" [:CodeBlock [:FunctionDec "function" [:WS " "] "(" ")" [:WS " "] "{"
+                                    [:CodeBlock [:WS "\n"]] "}"]]
+    [:frag ["line" ["keyword" "function"] " () {"] "\n" ["line" "}"]]]
+   ["function () {  }" [:CodeBlock [:FunctionDec "function" [:WS " "] "(" ")" [:WS " "] "{"
+                                    [:CodeBlock [:WS "  "]] "}"]]
+    ["line" ["keyword" "function"] " () {  }"]]
+   ["function what() { }" [:CodeBlock
+                           [:FunctionDec "function" [:WS " "] [:Identifier "what"] "(" ")"
+                            [:WS " "] "{" [:CodeBlock [:WS " "]] "}"]]
+    ["line" ["keyword" "function"] " " ["identifier" "what"] "() { }"]]
+   ["var what" [:CodeBlock [:VarDec "var" [:WS " "] [:Identifier "what"]]]
+    ["line" ["keyword" "var"] " " ["identifier" "what"]]]
+   ["const what" [:CodeBlock [:VarDec "const" [:WS " "] [:Identifier "what"]]]
+    ["line" ["keyword" "const"] " " ["identifier" "what"]]]
+   ["let what" [:CodeBlock [:VarDec "let" [:WS " "] [:Identifier "what"]]]
+    ["line" ["keyword" "let"] " " ["identifier" "what"]]]
+   ["var what = 5" [:CodeBlock
+                    [:VarDec "var" [:WS " "] [:Identifier "what"] [:WS " "] "=" [:WS " "] [:Number "5"]]]
+    ["line" ["keyword" "var"] " " ["identifier" "what"] " = " ["number" "5"]]]
+   ["const [a, b]=what" [:CodeBlock [:VarDec "const" [:WS " "]
+                                     [:Array "[" [:Identifier "a"] "," [:WS " "] [:Identifier "b"] "]"]
+                                     "=" [:Identifier "what"]]]
+    ["line" ["keyword" "const"] " [" ["identifier" "a"] ", " ["identifier" "b"] "]=" ["identifier" "what"]]]
+   ["const [a, b] = do(5)" [:CodeBlock [:VarDec "const" [:WS " "]
+                                        [:Array "[" [:Identifier "a"] "," [:WS " "] [:Identifier "b"] "]"]
+                                        [:WS " "] "=" [:WS " "]
+                                        [:Invoke [:Identifier "do"] "(" [:Number "5"] ")"]]]
+    ["line" ["keyword" "const"] " [" ["identifier" "a"] ", " ["identifier" "b"] "] = " ["identifier" "do"] "(" ["number" "5"] ")"]]
+   ["function () { var what }" [:CodeBlock [:FunctionDec "function" [:WS " "] "(" ")" [:WS " "] "{"
+                                            [:CodeBlock [:WS " "]
+                                             [:VarDec "var" [:WS " "] [:Identifier "what"] [:WS " "]]]
+                                            "}"]]
+    ["line" ["keyword" "function"] " () { " ["keyword" "var"] " " ["identifier" "what"] " }"]]
+   ["var one\nvar two" [:CodeBlock
+                        [:VarDec "var" [:WS " "] [:Identifier "one"] [:WS "\n"]]
+                        [:VarDec "var" [:WS " "] [:Identifier "two"]]]
+    [:frag ["line" ["keyword" "var"] " " ["identifier" "one"]] "\n" ["line" ["keyword" "var"] " " ["identifier" "two"]]]]
+   ["return 5" [:CodeBlock [:Return "return" [:WS " "] [:Number "5"]]]
+    ["line" ["keyword" "return"] " " ["number" "5"]]]
+   ["// what\n" [:CodeBlock [:WS [:Comment "//" " what"] "\n"]]
+    [:frag ["line" ["comment" "// what"]] "\n" ["line"]]]
+   ["5" [:CodeBlock [:Number "5"]]
+    ["line" ["number" "5"]]]
+   ["what()" [:CodeBlock [:Invoke [:Identifier "what"] "(" ")"]]
+    ["line" ["identifier" "what"] "()"]]
+   ["what(5)" [:CodeBlock [:Invoke [:Identifier "what"] "(" [:Number "5"] ")"]]
+    ["line" ["identifier" "what"] "(" ["number" "5"] ")"]]
+   ["foo.bar(5)" [:CodeBlock [:Invoke [:PropAccess [:Identifier "foo"] "." [:Identifier "bar"]] "(" [:Number "5"] ")"]]
+    ["line" ["identifier" "foo"] "." ["identifier" "bar"] "(" ["number" "5"] ")"]]
+   ["<div />" [:CodeBlock [:JSX [:TagClosed "<" [:Identifier "div"] [:WS " "] "/>"]]]
+    ["line" "&lt;" ["identifier" "div"] " />"]]
+   ["<div>text</div>" [:CodeBlock [:JSX [:Tag "<" [:Identifier "div"] ">" [:PlainText "text"] "</" [:Identifier "div"] ">"]]]
+    ["line" "&lt;" ["identifier" "div"] ">text&lt;/" ["identifier" "div"] ">"]]
+   ["<div>{5}</div>"
+    [:CodeBlock [:JSX [:Tag "<" [:Identifier "div"] ">" [:JS "{" [:Number "5"] "}"] "</" [:Identifier "div"] ">"]]]
+    ["line" "&lt;" ["identifier" "div"] ">{" ["number" "5"] "}&lt;/" ["identifier" "div"] ">"]]
+   ["<div id=\"a\" />"
+    [:CodeBlock [:JSX [:TagClosed "<" [:Identifier "div"] [:WS " "] [:Prop "id" "=" "\"" "a" "\""] [:WS " "] "/>"]]]
+    ["line" "&lt;" ["identifier" "div"] " " ["property" "id"] "=\"a\" />"]]
+   ["<div id={5} />" [:CodeBlock [:JSX [:TagClosed "<" [:Identifier "div"] [:WS " "]
+                                        [:Prop "id" "=" [:JS "{" [:Number "5"] "}"]] [:WS " "] "/>"]]]
+    ["line" "&lt;" ["identifier" "div"] " " ["property" "id"] "={" ["number" "5"] "} />"]]
+   ["<div id={5}>foobar</div>"
+    [:CodeBlock [:JSX [:Tag "<" [:Identifier "div"] [:WS " "] [:Prop "id" "=" [:JS "{" [:Number "5"] "}"]] ">"
+                       [:PlainText "foobar"] "</" [:Identifier "div"] ">"]]]
+    ["line" "&lt;" ["identifier" "div"] " " ["property" "id"] "={" ["number" "5"] "}>foobar</" ["identifier" "div"] ">"]]
+   ["<div><div>foo</div></div>"
+    [:CodeBlock [:JSX [:Tag "<" [:Identifier "div"] ">"
+                       [:Tag "<" [:Identifier "div"] ">" [:PlainText "foo"] "</" [:Identifier "div"] ">"]
+                       "</" [:Identifier "div"] ">"]]]
+    ["line" "&lt;" ["identifier" "div"] ">&lt;"
+     ["identifier" "div"] ">foo&lt;/" ["identifier" "div"] ">&lt;/" ["identifier" "div"] ">"]]
+   ["foo.bar.baz" [:CodeBlock [:PropAccess [:Identifier "foo"] "." [:Identifier "bar"] "." [:Identifier "baz"]]]
+    ["line" ["identifier" "foo"] "." ["identifier" "bar"] "." ["identifier" "baz"]]]
+   ["5 <= 2" [:CodeBlock [:Binary [:Number "5"] [:WS " "] "<=" [:WS " "] [:Number "2"]]]
+    ["line" ["number" "5"] " <= " ["number" "2"]]]])
 
-(deftest parse-tests-auto
+(deftest parse-tests
   (testing "parses various code"
     (doseq [[code parsed] cases]
       (is (= parsed (parse code))))))
+
+(deftest getHtml-test-auto
+  (doseq [c cases]
+    (if (= 3 (count c)) (testing (str "generates html for " (nth c 0)) (is (= (spans (nth c 2)) (getHtml (parse (nth c 0)))))))))
+
+(comment
+  (deftest parser-tests
+    (testing "parses various code"
+      (is (= [:Foo] ((parser [[:Foo "foo"]]) "foo"))))))
 
 (deftest getCode-tests
   (testing "getCode recreates the string from a parse"
@@ -84,7 +122,7 @@
     (is (= (getHtml (parse "var what")) (spans ["line" ["keyword" "var"] " " ["identifier" "what"]])))
     (is (= (getHtml (parse "var what = 5")) (spans ["line" ["keyword" "var"] " " ["identifier" "what"] " = " ["number" "5"]])))
     (is (= (getHtml (parse "var what\nvar who")) (spans [:frag
-                                                         ["line" ["keyword" "var"] " " ["identifier" "what"]] "<br />\n"
+                                                         ["line" ["keyword" "var"] " " ["identifier" "what"]] "\n"
                                                          ["line" ["keyword" "var"] " " ["identifier" "who"]]])))))
 
 (deftest localize-tests
